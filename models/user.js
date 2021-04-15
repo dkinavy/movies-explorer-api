5e63e3a84715acdb8f1d05c2d589d77354a6d4cd
dvkin <dennis.kinyaev@gmail.com>
2021-04-15 14:56:28 +0300

    user and movie schema added

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+var validator = require("validator");
+const BadRequestError = require("../errors/BadRequestError");
+
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Введите корректный Email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+    minlength: 6,
+  },
+
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        const r = new BadRequestError("Не найден такой пользователь");
+        throw r;
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          const r = new BadRequestError("Не найден такой пользователь");
+          throw r;
+        }
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model("user", userSchema);
